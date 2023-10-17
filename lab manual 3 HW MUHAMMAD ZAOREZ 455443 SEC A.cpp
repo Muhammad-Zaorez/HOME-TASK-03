9b17051742f9e833d8c84f9c2c835abb5bae295a
Muhammad-Zaorez <147588674+Muhammad-Zaorez@users.noreply.github.com>
2023-10-17 09:07:37 -0700

    Add files via upload

---
+++ lab manual 3 HW MUHAMMAD ZAOREZ 455443 SEC A.cpp
@@ -0,0 +1,130 @@
+/*
+                                LAB 3 HOMEWORK
+                                MUHAMMAD ZAOREZ
+                                    455443
+                                    SEC A
+*/
+#include <iostream>
+using namespace std;
+int main() {
+    /*int punjabPopulation = 120000000;
+    int sindhPopulation = 47890000;
+    int kpkPopulation = 3450000;
+    int balochistanPopulation = 1190000;
+    int choice;
+    cout << "Select a province to get its population:" << endl;
+    cout << "1. Punjab" << endl;
+    cout << "2. Sindh" << endl;
+    cout << "3. KPK" << endl;
+    cout << "4. Balochistan" << endl;
+    cout << "Enter your choice (1-4): ";
+    cin >> choice;
+
+    switch (choice) {
+        case 1:
+            cout << "Population of Punjab: " << punjabPopulation << endl;
+            break;
+        case 2:
+            cout << "Population of Sindh: " << sindhPopulation << endl;
+            break;
+        case 3:
+            cout << "Population of KPK: " << kpkPopulation << endl;
+            break;
+        case 4:
+            cout << "Population of Balochistan: " << balochistanPopulation << endl;
+            break;
+        default:
+            cout << "Invalid choice. Please select a valid option (1-4)." << endl;
+    }
+
+    return 0;*/
+    
+    //                             HOME TASK 02
+    /*char alphabet;
+    cout << "Enter an alphabet in small: ";
+    cin >> alphabet;
+
+    switch(alphabet) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+            cout << "The alphabet is a vowel.";
+            break;
+        default:
+            cout << "The alphabet is a consonant.";
+    }
+
+    return 0;*/
+    
+	//                              HOME TASK 03
+/*	int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    switch(number > 0) {
+        case true:
+            cout << "The number is positive.";
+            break;
+        case false:
+            switch(number < 0) {
+                case true:
+                    cout << "The number is negative.";
+                    break;
+                case false:
+                    cout << "The number is zero.";
+            }
+            break;
+    return 0;        
+}*/
+
+//                             HOME TASK 04
+/*int age;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    if (age >= 18) {
+        cout << "You are an adult.";
+    } else {
+        if (age >= 13 && age <= 17) {
+            cout << "You are a teenager.";
+        } else {
+            cout << "You are a child go home.";
+        }
+    }
+
+    return 0;*/
+    
+//                          HOME TASK 05
+/*int num1, num2, num3;
+    cout << "Enter three numbers: "<<endl;
+
+    cin >> num1 >> num2 >> num3;
+    if(num1 >= num2)
+    {
+        if(num1 >= num3)
+            cout <<num1 << " is the largest number";
+        else
+             cout <<num3 << " is the largest number";
+    }
+
+    else if(num2 >= num3)
+        cout <<num2 << " is the largest number";
+
+    else
+        cout <<num3 << " is the largest number";
+
+    return 0;*/
+    
+ //                            LAB TASK 06
+/* char ch;
+    cout<<"Enter an Alphabet: ";
+    cin>>ch;
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+        cout<<"\it is a vowel";
+    else if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U')
+        cout<<"\nIt is a Vowel";
+    else
+        cout<<"It is a consonant";
+    return 0;    
+}
